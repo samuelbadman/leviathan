f5386cdb3c3f20b19d6e8d256f9fe9b1ab23ee54
samuelbadman <samuelbadman@outlook.com>
2025-07-29 12:23:16 +0100

    Removed delegate development code

--- Source/GameTitle/TitleApplication.cpp
+++ Source/GameTitle/TitleApplication.cpp
@@ -3,69 +3,9 @@
 
 IMPLEMENT(TitleApplication)
 
-void Free()
-{
-	CONSOLE_PRINTF("free print\n");
-}
-
-void FreeP(int32_t N)
-{
-	CONSOLE_PRINTF("free print %d\n", N);
-}
-
 TitleApplication::TitleApplication(Core::Engine& EngineInstanceRunningApplication)
 	: Core::Application(EngineInstanceRunningApplication)
 {
 	GetEngine().CreateConsoleWindow();
-	CONSOLE_PRINTF("hello title application\n\n");
-
-	SD.BindFunction(&Free);
-	SD.BindMethod<TitleApplication, &TitleApplication::Print>(this);
-	SD.BindLambda([]() { CONSOLE_PRINTF("lambda print\n"); });
-
-	SDP.BindFunction(&FreeP);
-	SDP.BindMethod<TitleApplication, &TitleApplication::PrintP>(this);
-	SDP.BindLambda([](int32_t N) { CONSOLE_PRINTF("lambda print %d\n", N); });
-
-	MD.AddFunction(&Free);
-	MD.AddFunction(&Free);
-	MD.AddMethod<TitleApplication, &TitleApplication::Print>(this);
-	MD.AddMethod<TitleApplication, &TitleApplication::Print>(this);
-	MD.AddLambda([]() { CONSOLE_PRINTF("lambda print\n"); });
-	MD.AddLambda([]() { CONSOLE_PRINTF("lambda print\n"); });
-
-	MDP.AddFunction(&FreeP);
-	MDP.AddFunction(&FreeP);
-	MDP.AddMethod<TitleApplication, &TitleApplication::PrintP>(this);
-	MDP.AddMethod<TitleApplication, &TitleApplication::PrintP>(this);
-	MDP.AddLambda([](int32_t N) { CONSOLE_PRINTF("lambda print %d\n", N); });
-	MDP.AddLambda([](int32_t N) { CONSOLE_PRINTF("lambda print %d\n", N); });
+	CONSOLE_PRINTF("hello title application\n");
 }
-
-void TitleApplication::Print()
-{
-	CONSOLE_PRINTF("title app method print\n");
-}
-
-void TitleApplication::PrintP(int32_t N)
-{
-	CONSOLE_PRINTF("title app method print %d\n", N);
-}
-
-void TitleApplication::Begin()
-{
-	SD.Execute();
-
-	CONSOLE_PRINTF("\n\n");
-
-	MD.Execute();
-
-	CONSOLE_PRINTF("\n\n");
-
-	SDP.Execute(15);
-
-	CONSOLE_PRINTF("\n\n");
-
-	MDP.Execute(20);
-}
-
--- Source/GameTitle/TitleApplication.h
+++ Source/GameTitle/TitleApplication.h
@@ -1,30 +1,9 @@
 #pragma once
 
 #include "Core/Application.h"
-#include "Core/Delegate.h"
-
-DECLARE_SINGLE_DELEGATE(TestSingleDelegate);
-DECLARE_MULTI_DELEGATE(TestMultiDelegate);
-
-DECLARE_SINGLE_DELEGATE_OneParam(TestSingleDelegateParam, int32_t);
-
-DECLARE_MULTI_DELEGATE_OneParam(TestMultiDelegateParam, int32_t);
 
 class TitleApplication : public Core::Application
 {
-private:
-	TestSingleDelegate SD = {};
-	TestSingleDelegateParam SDP = {};
-	TestMultiDelegate MD = {};
-	TestMultiDelegateParam MDP = {};
-
 public:
 	TitleApplication(Core::Engine& EngineInstanceRunningApplication);
-
-private:
-	void Print();
-	void PrintP(int32_t N);
-
-	virtual void Begin() override;
-
 };
