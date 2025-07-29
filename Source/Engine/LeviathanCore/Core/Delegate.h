9881dde6352c72adba3ff6b43cc8cec20c359055
samuelbadman <samuelbadman@outlook.com>
2025-07-29 12:22:14 +0100

    Added one parameter delegate declaration definitions

--- Source/Engine/LeviathanCore/Core/Delegate.h
+++ Source/Engine/LeviathanCore/Core/Delegate.h
@@ -82,6 +82,88 @@
 		}\
 	};
 
+#define DECLARE_SINGLE_DELEGATE_OneParam(Name, Param1Type)\
+	class Name\
+	{\
+	private:\
+		using FunctionStubType = void(*)(Param1Type);\
+		using MethodStubType = void(*)(void* object_ptr, Param1Type);\
+		using LambdaStubType = void(*)(Param1Type);\
+\
+		FunctionStubType FunctionStubPtr = nullptr;\
+\
+		void* ObjPtr = nullptr;\
+		MethodStubType MethodStubPtr = nullptr;\
+\
+		LambdaStubType LambdaStubPtr = nullptr;\
+\
+		template <class T, void (T::* Method)(Param1Type)>\
+		static void MethodStub(void* ObjectPtr, Param1Type Param1)\
+		{\
+			T* pObj = static_cast<T*>(ObjectPtr);\
+			return (pObj->*Method)(Param1);\
+		}\
+\
+	public:\
+		void BindFunction(void(*Function)(Param1Type))\
+		{\
+			FunctionStubPtr = Function;\
+		}\
+\
+		void UnbindFunction()\
+		{\
+			FunctionStubPtr = nullptr;\
+		}\
+\
+		template <class T, void (T::* Method)(Param1Type)>\
+		void BindMethod(T* ObjectPtr)\
+		{\
+			ObjPtr = static_cast<void*>(ObjectPtr);\
+			MethodStubPtr = &MethodStub<T, Method>;\
+		}\
+\
+		void UnbindMethod()\
+		{\
+			ObjPtr = nullptr;\
+			MethodStubPtr = nullptr;\
+		}\
+\
+		void BindLambda(void(*Lambda)(Param1Type))\
+		{\
+			LambdaStubPtr = Lambda;\
+		}\
+\
+		void UnbindLambda()\
+		{\
+			LambdaStubPtr = nullptr;\
+		}\
+\
+		void Clear()\
+		{\
+			UnbindFunction();\
+			UnbindMethod();\
+			UnbindLambda();\
+		}\
+\
+		void Execute(Param1Type Param1)\
+		{\
+			if (FunctionStubPtr)\
+			{\
+				FunctionStubPtr(Param1);\
+			}\
+\
+			if (ObjPtr && MethodStubPtr)\
+			{\
+				(*MethodStubPtr)(ObjPtr, Param1);\
+			}\
+\
+			if (LambdaStubPtr)\
+			{\
+				LambdaStubPtr(Param1);\
+			}\
+		}\
+	};
+
 #define DECLARE_MULTI_DELEGATE(Name)\
 	class Name\
 	{\
@@ -164,6 +246,93 @@
 				if (Stub)\
 				{\
 					Stub();\
+				}\
+			}\
+		}\
+	};
+
+#define DECLARE_MULTI_DELEGATE_OneParam(Name, Param1Type)\
+	class Name\
+	{\
+	private:\
+		using FunctionStubType = void(*)(Param1Type);\
+		using MethodStubType = void(*)(void* object_ptr, Param1Type);\
+		using LambdaStubType = void(*)(Param1Type);\
+\
+		std::vector<FunctionStubType> FunctionStubPtrs = {};\
+		std::vector<std::pair<void*, MethodStubType>> MethodStubPtrs = {};\
+		std::vector<LambdaStubType> LambdaStubPtrs = {};\
+\
+		template <class T, void (T::* Method)(Param1Type)>\
+		static void MethodStub(void* ObjectPtr, Param1Type Param1)\
+		{\
+			T* pObj = static_cast<T*>(ObjectPtr);\
+			return (pObj->*Method)(Param1);\
+		}\
+\
+	public:\
+		void AddFunction(void(*Function)(Param1Type))\
+		{\
+			FunctionStubPtrs.push_back(Function);\
+		}\
+\
+		void RemoveFunction(void(*Function)(Param1Type))\
+		{\
+			FunctionStubPtrs.erase(std::remove(FunctionStubPtrs.begin(), FunctionStubPtrs.end(), Function));\
+		}\
+\
+		template <class T, void (T::* Method)(Param1Type)>\
+		void AddMethod(T* ObjectPtr)\
+		{\
+			MethodStubPtrs.emplace_back(static_cast<void*>(ObjectPtr), &MethodStub<T, Method>);\
+		}\
+\
+		template <class T, void (T::* Method)(Param1Type)>\
+		void RemoveMethod(T* ObjectPtr)\
+		{\
+			MethodStubPtrs.erase(std::remove(MethodStubPtrs.begin(), MethodStubPtrs.end(), std::pair<void*, MethodStubType>(static_cast<void*>(ObjectPtr), &MethodStub<T, Method>)));\
+		}\
+\
+		void AddLambda(void(*Lambda)(Param1Type))\
+		{\
+			LambdaStubPtrs.push_back(Lambda);\
+		}\
+\
+		void RemoveLambda(void(*Lambda)(Param1Type))\
+		{\
+			LambdaStubPtrs.erase(std::remove(LambdaStubPtrs.begin(), LambdaStubPtrs.end(), Lambda));\
+		}\
+\
+		void Clear()\
+		{\
+			FunctionStubPtrs.clear();\
+			MethodStubPtrs.clear();\
+			LambdaStubPtrs.clear();\
+		}\
+\
+		void Execute(Param1Type Param1)\
+		{\
+			for (const FunctionStubType& Stub : FunctionStubPtrs)\
+			{\
+				if (Stub)\
+				{\
+					Stub(Param1);\
+				}\
+			}\
+\
+			for (const std::pair<void*, MethodStubType>& Stub : MethodStubPtrs)\
+			{\
+				if (Stub.first && Stub.second)\
+				{\
+					(*Stub.second)(Stub.first, Param1);\
+				}\
+			}\
+\
+			for (const LambdaStubType& Stub : LambdaStubPtrs)\
+			{\
+				if (Stub)\
+				{\
+					Stub(Param1);\
 				}\
 			}\
 		}\
